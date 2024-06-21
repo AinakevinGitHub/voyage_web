23ace7a3fc6051bcead86aee707445d264c58476
AinakevinGitHub <x314571@gmail.com>
2024-06-21 20:11:45 +0300

    first commit

---
+++ src/main/java/com/example/voyage_web/VoyageWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.voyage_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoyageWebApplication.class, args);
+    }
+
+}
